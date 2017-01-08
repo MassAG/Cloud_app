8dddb88c49b4efb5f49bcfcd414f854f9c04058d
Massilia <aitgherbim@eisti.eu>
2017-01-08 22:51:55 +0100

    adding vm management

--- app/controllers/vms_controller.rb
+++ app/controllers/vms_controller.rb
@@ -4,7 +4,21 @@ class VmsController < ApplicationController
 
   def create
     @vm = current_user.vms.build(vm_params)
+    server_root_path=Rails.root.to_s
+    vms_config_file=server_root_path+"/vms/Vagrantfile"
     if @vm.save
+      server_vms_path=server_root_path+"/vms/"+@vm.id.to_s
+      puts "VMs path = #{server_vms_path}"
+      line = Command::Runner.new("mkdir",server_vms_path.to_s)
+      message = line.pass
+      puts "stdout " + message.stdout
+      puts "line " + message.line
+      line = Command::Runner.new("cp",vms_config_file.to_s+" "+server_vms_path.to_s)
+      message = line.pass
+      puts "line " + message.line
+      system("VAGRANT_CWD="+server_vms_path.to_s+" vagrant up")
+
+#      debugger
       flash[:success] = "VM created!"
       redirect_to root_url
     else
@@ -15,6 +29,10 @@ class VmsController < ApplicationController
 
   def destroy
     @vm.destroy
+    server_root_path=Rails.root.to_s
+    server_vms_path=server_root_path+"/vms/"+@vm.id.to_s
+    system("VAGRANT_CWD="+server_vms_path.to_s+" vagrant destroy -f")
+    system("rm -r "+server_vms_path.to_s)
     flash[:success] = "VM deleted"
     redirect_to request.referrer || root_url
   end
